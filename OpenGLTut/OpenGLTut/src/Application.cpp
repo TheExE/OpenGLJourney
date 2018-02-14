8bf96941d2a04f45b388d501aa9e9fd26de9f6ce
Martins <kalninsmartins1@gmail.com>
2018-02-14 09:50:07 +0200

    - Introduction of vertex array object (VAO).

--- OpenGLTut/OpenGLTut/src/Application.cpp
+++ OpenGLTut/OpenGLTut/src/Application.cpp
@@ -125,6 +125,10 @@ int main()
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Super duper cool window !", NULL, NULL);
 	if (!window)
@@ -159,6 +163,12 @@ int main()
 		2, 3, 0
 	};
 
+
+	// Bind vertex object array
+	unsigned int vaoId;
+	GLCall(glGenVertexArrays(1, &vaoId));
+	GLCall(glBindVertexArray(vaoId));
+
 	// Position buffer
 	unsigned int bufferId;
 	GLCall(glGenBuffers(1, &bufferId));
@@ -166,15 +176,15 @@ int main()
 	GLCall(glBufferData(GL_ARRAY_BUFFER, SIZE * sizeof(float),
 		positions, GL_STATIC_DRAW));
 
-	// Indicies buffer
-	unsigned int ibo;
-	GLCall(glGenBuffers(1, &ibo));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int),
-		indices, GL_STATIC_DRAW));
-
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr));
+
+	// Index buffer
+	unsigned int indexBufferId;
+	GLCall(glGenBuffers(1, &indexBufferId));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int),
+		indices, GL_STATIC_DRAW));
 
 	const ShaderProgramSoruce& shaderProgram =
 		ParseShader("res/shaders/Basic.shader");
@@ -186,6 +196,10 @@ int main()
 	GLCall(const int location = glGetUniformLocation(shaderProgrammId, "u_color"));
 	ASSERT(location != -1);
 	
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -196,7 +210,12 @@ int main()
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUseProgram(shaderProgrammId));
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+		GLCall(glBindVertexArray(vaoId));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		r += increment;
